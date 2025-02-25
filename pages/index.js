32398513f15be9c8c0191a9770e3369da90f3901
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-02-26 00:10:22 +0500

    feat: routes, pages and navigation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <h1>Hello Pages Router</h1>;
+import Link from "next/link";
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello Pages Router</h1>
+
+      <div>
+        <Link href="/cabins">Cabins</Link>
+        <Link href="/about">About</Link>
+      </div>
+    </div>
+  );
 }
+
+export default Home;
