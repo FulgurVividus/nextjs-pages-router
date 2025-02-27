e4543643d0cd699c18204560b582dc217eb983bc
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-02-28 00:16:01 +0500

    feat: fetching data with getStaticProps (SSG)

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://cpbkfsuxkwclxynwyhmr.supabase.co",
+        hostname: "cpbkfsuxkwclxynwyhmr.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
