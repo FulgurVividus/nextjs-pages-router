98e12991ce1fcc7f9b5843ab50b9c5a46b10fc27
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-02-27 21:34:27 +0500

    feat: defined Page Title and Favicon

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 function Cabin() {
   const router = useRouter();
 
-  return <div>Cabin #{router.query.cabinId}</div>;
+  return (
+    <>
+      <Head>
+        <title>The Wild Oasis | Cabin #{router.query.cabinId}</title>
+      </Head>
+      <div>Cabin #{router.query.cabinId}</div>
+    </>
+  );
 }
 
 export default Cabin;
